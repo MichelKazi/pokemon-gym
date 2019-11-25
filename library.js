dc6978ac2b435a237b547317990fc5f5662dc92b
Michel Kazi <michel.m.kazi@gmail.com>
2019-11-25 16:53:59 -0500

    added API call

--- library.js
+++ library.js
@@ -171,5 +171,31 @@ class Pokemon {
         this.types.push(type)
     }
 }
-export default Trainer
 
+queryData = async (id) => {
+    let response
+    
+    try {
+        response = await axios.get(`https://fizal.me/pokeapi/api/v2/${isNaN(id) ? 'name/' : 'id/'}${id}.json`)
+        const acquiredPokemon = response.data
+        
+        this.name = acquiredPokemon.name
+        this.id = acquiredPokemon.id
+        this.hp = acquiredPokemon.stats[5].base_stat// For every single Pokemon JSON, 
+        this.attack = acquiredPokemon.stats[4].base_stat // these indices for stats will
+        this.defense = acquiredPokemon.stats[3].base_stat // always be the same 
+        this.weight = acquiredPokemon.weight
+        this.spriteURL = acquiredPokemon.sprites.front_default
+        
+        this.abilities = acquiredPokemon.abilities.map(abilities => {
+            return abilities.ability.name.replace('-', ' ')
+        })
+        
+        return Promise.resolve(this)
+    }
+    catch (err) {
+        console.error(err)
+    }
+    
+}
+export default library
