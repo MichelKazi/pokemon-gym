855b2efd3283824cb985c753751dd99e5832ef89
Brian <brianpeguero@gmail.com>
2019-11-25 16:55:30 -0500

    added more features to the animations.js file

--- animations.js
+++ animations.js
@@ -1,3 +1,29 @@
-let BrianGrid = document.getElementById('brian-grid')
-let michelGrid = docuent.getElementById('michel-grid')
+let brianGrid = document.getElementById('brian-grid')
+let michelGrid = document.getElementById('michel-grid')
 let erinGrid = document.getElementById('erin-grid')
+
+let mainGrid = document.getElementById('main-grid')
+
+brianGrid.addEventListener('click', () => {
+    for(let i = 0; i < 80; i++) {
+        setTimeout(() => {
+            mainGrid.style.gridTemplateColumns = `${i}% 10% 10%`
+        }, i)
+    }
+})
+
+michelGrid.addEventListener('click', () => {
+    for(let i = 0; i < 80; i++) {
+        setTimeout(() => {
+            mainGrid.style.gridTemplateColumns = `$10% ${i}% 10%`
+        }, i)
+    }
+})
+
+erinGrid.addEventListener('click', () => {
+    for(let i = 0; i < 80; i++) {
+        setTimeout(() => {
+            mainGrid.style.gridTemplateColumns = `$10% 10% ${i}%`
+        }, i)
+    }
+})
