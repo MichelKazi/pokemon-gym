c127095b10f5b744753f2ef4ea058f4a0d6963dd
Michel Kazi <michel.m.kazi@gmail.com>
2019-11-25 16:13:32 -0500

    removed binding of 'this' keyword since this isn't react lol

--- library.js
+++ library.js
@@ -3,25 +3,24 @@ export class Trainer {
         this.name = name
         this.team = Array.size(3).fill(0)
         //binding this keyword for methods
-        this.all = this.all.bind(this)
-        this.addPokemon = this.addPokemon.bind(this)
-        this.get = this.get.bind(this)
-        this.equip = this.equip.bind(this)
+        // this.all = this.all.bind(this)
+        // this.addPokemon = this.addPokemon.bind(this)
+        // this.get = this.get.bind(this)
+        // this.equip = this.equip.bind(this)
     }
 
     all() {
-        return this.pokemonList
+        return this.team
     }
 
     addPokemon(pokemon) {
-        if (this.pokemonList.length > 4) {
+        if (this.team.length > 4) {
             return console.error(`${this.name}'s team is full!`);
         }
         if (!this.equipped) return this.equipped = pokemon
         else {
 
-            this.pokemonList.push(pokemon)
-            this.pokeballs--
+            this.team.push(pokemon)
         }
 
     }
@@ -29,9 +28,8 @@ export class Trainer {
 
     removePokemon(pokemon) {
 
-        this.pokemonList = this.pokemonList.filter(p => p.id !== pokemon.id)
+        this.team = this.team.filter(p => p.id !== pokemon.id)
 
-        this.pokeballs++
     }
 
 }
