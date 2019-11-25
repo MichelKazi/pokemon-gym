e6295ff137abd36593f265d3a49fbec05009826f
Brian <brianpeguero@gmail.com>
2019-11-25 16:06:38 -0500

    created an animations file

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Hey lol
 
 - When you're contributing, please do ``` git checkout dev ```
+
+Made another change to see if it works
---
+++ animations.js
@@ -0,0 +1,3 @@
+let BrianGrid = document.getElementById('brian-grid')
+let michelGrid = docuent.getElementById('michel-grid')
+let erinGrid = document.getElementById('erin-grid')
