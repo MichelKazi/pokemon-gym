7ef2d3cb07a98672dbd4ec8bd0d8e13e0401e2c0
Michelkazi <michel.m.kazi@gmail.com>
2019-11-26 15:43:40 -0500

    fixed index.html

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 
+
 const erin = new Trainer('Erin')
 const brian = new Trainer('Brian')
 const michel = new Trainer('Michel')
 
 
+const trainers = [erin, brian, michel]
 
-const [ erin, brian, michel ] = trainers
 
-
+erin.addPokemon(createPokemon("noivern"))
+erin.addPokemon(createPokemon("dragalge"))
+erin.addPokemon(createPokemon("kommo-o"))
