56b5517f0ae5e37255ede7df640874913c0e70bc
Brian <brianpeguero@gmail.com>
2019-11-25 21:24:41 -0500

    created the animation for when the background of the specific trainer is clicked

--- animations.js
+++ animations.js
@@ -4,26 +4,66 @@ let erinGrid = document.getElementById('erin-grid')
 
 let mainGrid = document.getElementById('main-grid')
 
-brianGrid.addEventListener('click', () => {
-    for(let i = 0; i < 80; i++) {
-        setTimeout(() => {
-            mainGrid.style.gridTemplateColumns = `${i}% 10% 10%`
-        }, i)
+michelGrid.addEventListener('click', () => {
+    if (mainGrid.style.gridTemplateColumns === `1.33% 99.33% 1.33%`) {
+        for(let i = 0; i < 67; i++) {
+            setTimeout(() => {
+                mainGrid.style.gridTemplateColumns = `33.33% ${99 - i}.33% 33.33%`
+            }, i * 2)
+        }
+    } else {
+        for(let i = 0; i < 34; i++) {
+            setTimeout(() => {
+                if (mainGrid.style.gridTemplateColumns === `1.33% 99.33% 1.33%`) {
+                    mainGrid.style.gridTemplateColumns = `1.33% 99.33% 1.33%`
+                } else {
+                    mainGrid.style.gridTemplateColumns = `${34 - i}.33% ${66 + i}.33% ${34 - i}.33%`
+                }
+            }, i * 2)
+        }
     }
+    console.log("you clicked on gym leader Michel")
+
 })
 
-michelGrid.addEventListener('click', () => {
-    for(let i = 0; i < 80; i++) {
-        setTimeout(() => {
-            mainGrid.style.gridTemplateColumns = `$10% ${i}% 10%`
-        }, i)
+brianGrid.addEventListener('click', () => {
+    if (mainGrid.style.gridTemplateColumns === `1.33% 1.33% 99.33%`) {
+        for(let i = 0; i < 67; i++) {
+            setTimeout(() => {
+                mainGrid.style.gridTemplateColumns = `33.33% 33.33% ${99 - i}.33%`
+            }, i * 2)
+        }
+    } else {
+        for(let i = 0; i < 34; i++) {
+            setTimeout(() => {
+                if (mainGrid.style.gridTemplateColumns === `1.33% 1.33% 99.33%`) {
+                    mainGrid.style.gridTemplateColumns = `1.33% 1.33% 99.33%`
+                } else {
+                    mainGrid.style.gridTemplateColumns = `${34 - i}.33% ${34 - i}.33% ${66 + i}.33%`
+                }
+            }, i * 2)
+        }
     }
+    console.log("you clicked on gym leader Brian")
 })
 
 erinGrid.addEventListener('click', () => {
-    for(let i = 0; i < 80; i++) {
-        setTimeout(() => {
-            mainGrid.style.gridTemplateColumns = `$10% 10% ${i}%`
-        }, i)
+    if (mainGrid.style.gridTemplateColumns === `99.33% 1.33% 1.33%`) {
+        for(let i = 0; i < 67; i++) {
+            setTimeout(() => {
+                mainGrid.style.gridTemplateColumns = `${99 - i}.33% 33.33% 33.33%`
+            }, i * 2)
+        }
+    } else {
+        for(let i = 0; i < 34; i++) {
+            setTimeout(() => {
+                if (mainGrid.style.gridTemplateColumns === `99.33% 1.33% 1.33%`) {
+                    mainGrid.style.gridTemplateColumns = `99.33% 1.33% 1.33%`
+                } else {
+                    mainGrid.style.gridTemplateColumns = `${66 + i}.33% ${34 - i}.33% ${34 - i}.33%`
+                }
+            }, i * 2)
+        }
     }
+    console.log("you clicked on gym leader Erin")
 })
