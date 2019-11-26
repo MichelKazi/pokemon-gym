576ced869ca9a6175b1c75965e64d9eeb9f05fa2
Brian <brianpeguero@gmail.com>
2019-11-25 21:37:30 -0500

    made the margins around the contains smaller

--- animations.js
+++ animations.js
@@ -5,21 +5,21 @@ let erinGrid = document.getElementById('erin-grid')
 let mainGrid = document.getElementById('main-grid')
 
 michelGrid.addEventListener('click', () => {
-    if (mainGrid.style.gridTemplateColumns === `1.33% 99.33% 1.33%`) {
+    if (mainGrid.style.gridTemplateColumns === `0.33% 99.33% 0.33%`) {
         for(let i = 0; i < 67; i++) {
             setTimeout(() => {
                 mainGrid.style.gridTemplateColumns = `33.33% ${99 - i}.33% 33.33%`
-            }, i * 2)
+            }, i * 5)
         }
     } else {
         for(let i = 0; i < 34; i++) {
             setTimeout(() => {
-                if (mainGrid.style.gridTemplateColumns === `1.33% 99.33% 1.33%`) {
-                    mainGrid.style.gridTemplateColumns = `1.33% 99.33% 1.33%`
+                if (mainGrid.style.gridTemplateColumns === `.33% 99.33% .33%`) {
+                    mainGrid.style.gridTemplateColumns = `.33% 99.33% .33%`
                 } else {
-                    mainGrid.style.gridTemplateColumns = `${34 - i}.33% ${66 + i}.33% ${34 - i}.33%`
+                    mainGrid.style.gridTemplateColumns = `${33 - i}.33% ${66 + i}.33% ${33 - i}.33%`
                 }
-            }, i * 2)
+            }, i * 5)
         }
     }
     console.log("you clicked on gym leader Michel")
@@ -27,42 +27,42 @@ michelGrid.addEventListener('click', () => {
 })
 
 brianGrid.addEventListener('click', () => {
-    if (mainGrid.style.gridTemplateColumns === `1.33% 1.33% 99.33%`) {
+    if (mainGrid.style.gridTemplateColumns === `0.33% 0.33% 99.33%`) {
         for(let i = 0; i < 67; i++) {
             setTimeout(() => {
                 mainGrid.style.gridTemplateColumns = `33.33% 33.33% ${99 - i}.33%`
-            }, i * 2)
+            }, i * 5)
         }
     } else {
         for(let i = 0; i < 34; i++) {
             setTimeout(() => {
-                if (mainGrid.style.gridTemplateColumns === `1.33% 1.33% 99.33%`) {
-                    mainGrid.style.gridTemplateColumns = `1.33% 1.33% 99.33%`
+                if (mainGrid.style.gridTemplateColumns === `.33% .33% 99.33%`) {
+                    mainGrid.style.gridTemplateColumns = `.33% .33% 99.33%`
                 } else {
-                    mainGrid.style.gridTemplateColumns = `${34 - i}.33% ${34 - i}.33% ${66 + i}.33%`
+                    mainGrid.style.gridTemplateColumns = `${33 - i}.33% ${33 - i}.33% ${66 + i}.33%`
                 }
-            }, i * 2)
+            }, i * 5)
         }
     }
     console.log("you clicked on gym leader Brian")
 })
 
 erinGrid.addEventListener('click', () => {
-    if (mainGrid.style.gridTemplateColumns === `99.33% 1.33% 1.33%`) {
+    if (mainGrid.style.gridTemplateColumns === `99.33% 0.33% 0.33%`) {
         for(let i = 0; i < 67; i++) {
             setTimeout(() => {
                 mainGrid.style.gridTemplateColumns = `${99 - i}.33% 33.33% 33.33%`
-            }, i * 2)
+            }, i * 5)
         }
     } else {
         for(let i = 0; i < 34; i++) {
             setTimeout(() => {
-                if (mainGrid.style.gridTemplateColumns === `99.33% 1.33% 1.33%`) {
-                    mainGrid.style.gridTemplateColumns = `99.33% 1.33% 1.33%`
+                if (mainGrid.style.gridTemplateColumns === `99.33% .33% .33%`) {
+                    mainGrid.style.gridTemplateColumns = `99.33% .33% .33%`
                 } else {
-                    mainGrid.style.gridTemplateColumns = `${66 + i}.33% ${34 - i}.33% ${34 - i}.33%`
+                    mainGrid.style.gridTemplateColumns = `${66 + i}.33% ${33 - i}.33% ${33 - i}.33%`
                 }
-            }, i * 2)
+            }, i * 5)
         }
     }
     console.log("you clicked on gym leader Erin")
