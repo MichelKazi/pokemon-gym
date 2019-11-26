ef7215e1f261aed6295609c8d7ed7eab627585fa
Michelkazi <michel.m.kazi@gmail.com>
2019-11-26 16:33:01 -0500

    changed selector names in indexHtml to make my life a lot easier

--- main.js
+++ main.js
@@ -8,6 +8,18 @@ const michel = new Trainer('Michel')
 const trainers = [erin, brian, michel]
 
 
-erin.addPokemon(createPokemon("noivern"))
-erin.addPokemon(createPokemon("dragalge"))
-erin.addPokemon(createPokemon("kommo-o"))
+brian.addPokemon(createPokemon("noivern"))
+brian.addPokemon(createPokemon("dragalge"))
+brian.addPokemon(createPokemon("kommo-o"))
+
+erin.addPokemon(createPokemon("dragonair"))
+erin.addPokemon(createPokemon("reshiram"))
+erin.addPokemon(createPokemon("altaria"))
+
+michel.addPokemon(createPokemon("dragonair"))
+michel.addPokemon(createPokemon("reshiram"))
+michel.addPokemon(createPokemon("altaria"))
+
+let brianFirst = document.getElementsByClassName('brian-first')
+let brianSecond = document.getElementsByClassName('brian-second')
+let brianThird = document.getElementsByClassName('brian-third')
