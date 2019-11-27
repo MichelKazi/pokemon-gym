61cd04d7a0488c41df732c459e6486f13ea43efe
Michelkazi <michel.m.kazi@gmail.com>
2019-11-27 08:30:51 -0500

    I could do the cool stuff I did in the last commit in 6 less lines

--- main.js
+++ main.js
@@ -1,58 +1,47 @@
-
-
 const erin = new Trainer('Erin')
 const brian = new Trainer('Brian')
 const michel = new Trainer('Michel')
 
-
 const trainers = [erin, brian, michel]
 
-
 getPokemonElements = (name) => {
-    return {
-        imgs: document.querySelectorAll(`.${name}-pokemon-imgs`),
-        names: document.querySelectorAll(`.${name}-pokemon-name`)
-    }
+  return {
+    imgs: document.querySelectorAll(`.${name}-pokemon-imgs`),
+    names: document.querySelectorAll(`.${name}-pokemon-name`)
+  }
 }
 
+
+// querying the HTML elements where our pokemon names and images go
 let brianDOMPokemon = getPokemonElements('brian')
 let erinDOMPokemon = getPokemonElements('erin')
 let michelDOMPokemon = getPokemonElements('michel')
 
+const trainersInDOM = [brianDOMPokemon, erinDOMPokemon, michelDOMPokemon]
+
+// making sure to wait for the promises to resolve 
+
 Promise.all([
-    brian.addPokemon("noivern"),
-    brian.addPokemon("dragalge"),
-    brian.addPokemon("kommo-o"),
-    
-erin.addPokemon("dragonair"),
-erin.addPokemon("reshiram"),
-erin.addPokemon("altaria"),
+  brian.addPokemon("noivern"),
+  brian.addPokemon("dragalge"),
+  brian.addPokemon("kommo-o"),
 
-michel.addPokemon("hydreigon"),
-michel.addPokemon("rayquaza"),
-michel.addPokemon("guzzlord")])
+  erin.addPokemon("dragonair"),
+  erin.addPokemon("reshiram"),
+  erin.addPokemon("altaria"),
 
-.then( ()=> {
-    brianDOMPokemon.names.forEach((element, i) => {  
-        element.innerText= brian.team[i].name
-    })
-    brianDOMPokemon.imgs.forEach((element, i) => {
+  michel.addPokemon("hydreigon"),
+  michel.addPokemon("rayquaza"),
+  michel.addPokemon("guzzlord")])
+
+  .then(() => {
+    for (i in trainersInDOM) {
+      trainersInDOM[i].names.forEach((element, i) => {
+        element.innerText = brian.team[i].name
+      })
+      trainersInDOM[i].imgs.forEach((element, i) => {
         element.src = brian.team[i].gif
-    })
-    erinDOMPokemon.names.forEach((element, i) => {  
-        element.innerText= erin.team[i].name
-    })
-    erinDOMPokemon.imgs.forEach((element, i) => {
-        element.src = erin.team[i].gif
-    })
-    michelDOMPokemon.names.forEach((element, i) => {  
-        element.innerText= michel.team[i].name
-    })
-    michelDOMPokemon.imgs.forEach((element, i) => {
-        element.src = michel.team[i].gif
-    })
-
+      })
     }
-    )
 
-    
+  })
