32c96996fe40fce7f8b81b527180ab31ea98157f
Brian <brianpeguero@gmail.com>
2019-11-27 10:52:45 -0500

    erin made a merge conflict

--- main.js
+++ main.js
@@ -1,69 +1,46 @@
-
-
 const erin = new Trainer('Erin')
 const brian = new Trainer('Brian')
 const michel = new Trainer('Michel')
-
-
 const trainers = [erin, brian, michel]
-
-
 getPokemonElements = (name) => {
-    return {
-        imgs: document.querySelectorAll(`.${name}-pokemon-imgs`),
-        names: document.querySelectorAll(`.${name}-pokemon-name`)
-    }
+  return {
+    imgs: document.querySelectorAll(`.${name}-pokemon-imgs`),
+    names: document.querySelectorAll(`.${name}-pokemon-name`)
+  }
 }
-
+// querying the HTML elements where our pokemon names and images go
 let brianDOMPokemon = getPokemonElements('brian')
 let erinDOMPokemon = getPokemonElements('erin')
 let michelDOMPokemon = getPokemonElements('michel')
-
+const trainersInDOM = [erinDOMPokemon, brianDOMPokemon, michelDOMPokemon]
+// making sure to wait for the promises to resolve 
 Promise.all([
-    brian.addPokemon("noivern"),
-    brian.addPokemon("dragalge"),
-    brian.addPokemon("kommo-o"),
-    
-<<<<<<< HEAD
-    erin.addPokemon("dragonair"),
-    erin.addPokemon("reshiram"),
-    erin.addPokemon("altaria"),
-
-    michel.addPokemon("dragonair"),
-    michel.addPokemon("reshiram"),
-    michel.addPokemon("altaria")
-])
-=======
-erin.addPokemon("dragonair"),
-erin.addPokemon("reshiram"),
-erin.addPokemon("altaria"),
-
-michel.addPokemon("hydreigon"),
-michel.addPokemon("rayquaza"),
-michel.addPokemon("guzzlord")])
->>>>>>> 913bbedaec717f8840da22bf621ea03d860f223d
-
-.then( ()=> {
-    brianDOMPokemon.names.forEach((element, i) => {  
-        element.innerText= brian.team[i].name
+  brian.addPokemon("noivern"),
+  brian.addPokemon("dragalge"),
+  brian.addPokemon("tyrantrum"),
+  erin.addPokemon("dragonair"),
+  erin.addPokemon("reshiram"),
+  erin.addPokemon("altaria"),
+  michel.addPokemon("hydreigon"),
+  michel.addPokemon("rayquaza"),
+  michel.addPokemon("garchomp")])
+  .then(() => {
+    trainersInDOM[0].names.forEach((element, i) => {
+      element.innerText = erin.team[i].name
     })
-    brianDOMPokemon.imgs.forEach((element, i) => {
-        element.src = brian.team[i].gif
+    trainersInDOM[0].imgs.forEach((element, i) => {
+      element.src = erin.team[i].gif
     })
-    erinDOMPokemon.names.forEach((element, i) => {  
-        element.innerText= erin.team[i].name
+    trainersInDOM[1].names.forEach((element, i) => {
+      element.innerText = brian.team[i].name
     })
-    erinDOMPokemon.imgs.forEach((element, i) => {
-        element.src = erin.team[i].gif
+    trainersInDOM[1].imgs.forEach((element, i) => {
+      element.src = brian.team[i].gif
     })
-    michelDOMPokemon.names.forEach((element, i) => {  
-        element.innerText= michel.team[i].name
+    trainersInDOM[2].names.forEach((element, i) => {
+      element.innerText = michel.team[i].name
     })
-    michelDOMPokemon.imgs.forEach((element, i) => {
-        element.src = michel.team[i].gif
+    trainersInDOM[2].imgs.forEach((element, i) => {
+      element.src = michel.team[i].gif
     })
-
-    }
-    )
-
-    
+  })
